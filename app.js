e569338322cb5fad3315519418cf544741e80e3e
vignesh <vigneshfornavy@gmail.com>
2023-12-04 13:22:35 +0530

    div siblgins

---
+++ app.js
@@ -0,0 +1,44 @@
+const heading = React.createElement(
+    "div",
+    {
+        id: "parent"
+    },
+    [
+        
+    React.createElement(
+        "div",
+        { id: "child" },
+        [React.createElement(
+            "h1",
+            {},
+            "Hai this is vignesh 1",
+        ),
+        React.createElement(
+            "h1",
+            {},
+            "Hai this is vignesh 2",
+        )
+    ]
+    ),
+    React.createElement(
+        "div",
+        { id: "child" },
+        [React.createElement(
+            "h1",
+            {},
+            "Hai this is vignesh 1",
+        ),
+        React.createElement(
+            "h1",
+            {},
+            "Hai this is vignesh 2",
+        )
+    ]
+    )
+    ]
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
+
